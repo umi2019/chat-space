ec3b56e7d36001d005228df475238d43b7fd9e02
umi2019 <uamanuma@gmail.com>
2019-02-08 21:12:45 +0900

    Make group function

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
   root 'messages#index'
+  resources :groups do
+    resources :messages
+  end
   resources :users, only: [:edit, :update]
 end
