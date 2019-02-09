376e6005df826d5485c2ebaf68b4a6970c9bf530
umi2019 <uamanuma@gmail.com>
2019-02-09 14:34:43 +0900

    Change root path

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,4 +1,20 @@
 class GroupsController < ApplicationController
+  before_action :set_group, only: [:edit, :update]
+
+  def index
+  end
+
+  def edit
+  end
+
+  def update
+    if @group.update(group_params)
+      redirect_to group_messages_path(@group), notice: "グループを編集しました"
+    else
+      render :edit
+    end
+  end
+
   def new
     @users = User.all
     @group = Group.new
@@ -17,5 +33,9 @@ class GroupsController < ApplicationController
 
   def group_params
     params.require(:group).permit(:name, {user_ids: []}  )
+  end
+
+  def set_group
+    @group = Group.find(params[:id])
   end
 end
