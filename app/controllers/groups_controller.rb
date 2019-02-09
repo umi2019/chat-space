4d6187f877af5d548897d2ba7ade3f26c983f095
umi2019 <uamanuma@gmail.com>
2019-02-09 13:01:31 +0900

    Add group create function

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,5 +1,21 @@
 class GroupsController < ApplicationController
   def new
     @users = User.all
+    @group = Group.new
+    # @group.users.build
+  end
+
+  def create
+    if Group.create(group_params)
+      redirect_to root_path, notice: "グループを作成しました"
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def group_params
+    params.require(:group).permit(:name, {user_ids: []}  )
   end
 end
