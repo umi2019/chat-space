9cb8676b85033565344306d134bc5a3dd40be946
umi2019 <uamanuma@gmail.com>
2019-02-11 19:59:33 +0900

    Edit message.js

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,8 +1,41 @@
 $(function() {
-  $('#send_message').on('submit', function(event) {
+  function buildHTML(message) {
+    var html = `<div class="message">
+                  <div class="message__top">
+                    <p class="message__top__name">${message.user_name}</p>
+                    <p class="message__top__date">${message.date}</p>
+                  </div>
+                  <p class="message__text">${message.text}</p>
+                </div>`;
+    return html;
+  }
+
+  $('#message-form').on('submit', function(event) {
     event.preventDefault();
 
-    message = $('#message_box').val();
-    console.log('pushed');
+    var formData = new FormData(this);
+    var url = $(this).attr('action');
+    $.ajax({
+      url: url,
+      type: 'POST',
+      dataType: 'json',
+      data: formData,
+      processData: false,
+      contentType: false
+    })
+    .done(function(data) {
+      var html = buildHTML(data);
+      $('.messages').append(html);
+      $('#message_box').val('');
+      debugger;
+      console.log("success");
+    })
+    .fail(function() {
+      console.log("error");
+    })
+    .always(function() {
+      $('#send_message').prop('disabled', false);
+      console.log("complete");
+    });
   });
 });
