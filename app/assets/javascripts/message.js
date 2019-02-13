3fbc51e9ca8ab12abf0a0781c44aa29410c24ca7
umi2019 <uamanuma@gmail.com>
2019-02-13 21:12:49 +0900

    Add auto reload

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,6 +1,6 @@
 $(document).on('turbolinks:load', function() {
-  function buildSendMessageHTML(message) {
-    var html = `<div class="message">
+  function buildMessageHTML(message) {
+    var html = `<div class="message" data-id=${message.id}>
                   <div class="message__top">
                     <p class="message__top__name">${message.user_name}</p>
                     <p class="message__top__date">${message.date}</p>
@@ -11,11 +11,44 @@ $(document).on('turbolinks:load', function() {
     return html;
   }
 
+  function updateMessages(){
+    var message_id = 0;
+
+    if($('.message')[0]){
+      message_id = $('.message:last').data('id') / 1;
+    }
+    console.log($('.message:last').data('id'));
+    $.ajax({
+      url: location.href,
+      type: 'GET',
+      dataType: 'json',
+      data: {message: {id: message_id}}
+    })
+    .done(function(messages) {
+      if (messages.length !== 0) {
+        messages.forEach(function(message) {
+          var html = buildMessageHTML(message);
+          $('.messages').append(html);
+        });
+        $('.messages').animate({scrollTop: $('.messages')[0].scrollHeight}, 'fast');
+      }
+      console.log("success");
+    })
+    .fail(function() {
+      console.log("error");
+    })
+    .always(function() {
+      console.log("complete");
+    });
+  }
   //最初から下までスクロール
   if($('.messages').length){
     $('.messages').scrollTop($('.messages')[0].scrollHeight);
   }
 
+  setInterval(function(){
+    updateMessages();
+  }, 5000);
 
   $('#message-form').on('submit', function(event) {
     event.preventDefault();
@@ -31,8 +64,10 @@ $(document).on('turbolinks:load', function() {
       contentType: false
     })
     .done(function(data) {
-      var html = buildSendMessageHTML(data);
-      $('.messages').append(html);
+      updateMessages();
+      // setTimeout(function(){}, 1000);
+      // var html = buildMessageHTML(data);
+      // $('.messages').append(html);
       $('.messages').animate({scrollTop: $('.messages')[0].scrollHeight}, 'fast');
     })
     .fail(function() {
--- app/views/messages/create.json.jbuilder
+++ app/views/messages/create.json.jbuilder
@@ -2,4 +2,5 @@ json.user_name  @message.user.name
 json.text  @message.body.to_s
 json.date  @message.created_at.strftime("%Y/%m/%d(%a) %H:%M:%S")
 json.image @message.image.url.to_s
+json.id @message.id
 
