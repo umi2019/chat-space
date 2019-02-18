ba1fbd2ac67c23480a8b312468f74ece0565b011
umi2019 <uamanuma@gmail.com>
2019-02-18 10:59:21 +0900

    Add asset_host

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -13,5 +13,5 @@ CarrierWave.configure do |config|
   }
 
   config.fog_directory  = 'umi-chat-space'
-  # config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/umi-chat-space'
+  config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/umi-chat-space'
 end
