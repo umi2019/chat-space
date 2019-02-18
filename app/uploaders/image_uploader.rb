9933888014b77b94fa2169bebbbd5d86305db019
umi2019 <uamanuma@gmail.com>
2019-02-18 15:17:26 +0900

    Edit uprose config

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -2,13 +2,7 @@ class ImageUploader < CarrierWave::Uploader::Base
   include CarrierWave::MiniMagick
   process resize_to_limit: [400, 400]
 
-  if Rails.env.development?
-    storage :file
-  elsif Rails.env.test?
-    storage :file
-  else
-    storage :fog
-  end
+  storage :fog
 
   def store_dir
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
