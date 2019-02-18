39da76db520d4401ae2e2ac36074aa46af7d4c5f
umi2019 <uamanuma@gmail.com>
2019-02-18 12:05:25 +0900

    Edit carrier wave

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -12,6 +12,6 @@ CarrierWave.configure do |config|
     region: 'ap-northeast-1'
   }
 
-  config.fog_directory  = 'umi-chat-space'
-  config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/umi-chat-space'
+  config.fog_directory  = 'umi-chat-space2'
+  config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/umi-chat-space2'
 end
